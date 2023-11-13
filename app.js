47de012038d4a38f9322673b8b631bf266ce9d81
jnavarro30 <jnavarro30@live.com>
2023-11-13 14:18:11 -0800

    checkpoint

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ import puppeteer from "puppeteer";
 import stores from "./stores.js";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(stores.albertsons);
+  await page.locator("#skip-main-content").fill("bananas");
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation()
 
-  await page.screenshot({ path: "./screenshots/store.jpg" });
+  const imagesGrid = "div[data-qa='srch-grd-pd']";
+  
+  await page.waitForSelector(imagesGrid);
 
+  
+
+  const data = await page.$$eval("div[data-qa='srch-grd-pd'] div[data-cross-sell-position='1']", products => products);
+  console.log(data, 'let"s see');
+
+  // await page.screenshot({ path: "./screenshots/store.jpg" });
   await browser.close();
 })();
